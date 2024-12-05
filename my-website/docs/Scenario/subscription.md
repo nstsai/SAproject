a54a5a81afdc56b9bdd97d609d3a5e9883bbbf5f
nstsai <atsailiafa@gmail.com>
2024-12-05 13:16:23 +0300

    message

---
+++ my-website/docs/Scenario/subscription.md
@@ -0,0 +1,53 @@
+---
+title: Оформление подписки
+sidebar_position: 1
+---
+# Сценарий оформления подписки
+
+```plantuml
+
+@startuml
+
+left to right direction
+
+actor "Пользователь" as u
+actor "Служба поддержки" as s
+
+package "Оформление подписки" {
+usecase "Открыть раздел подписка" as UC1
+usecase "Изучить условия подписки" as UC2
+usecase "Выбрать тип подписки" as UC3
+usecase "Задать вопрос поддержке" as UC4
+usecase "Ввести данные для оплаты" as UC5
+usecase "Ввести номер карты" as UC6
+usecase "Ввести срок действия карты" as UC7
+usecase "Ввести CVC-код" as UC8
+usecase "Оплатить подписку" as UC9
+usecase "Оформить подписку" as UC10
+usecase "Отменить подписку" as UC11
+usecase "Получить вопрос от клиента" as UC12
+usecase "Ответить на вопрос клиента" as UC13
+}
+
+u --> UC1
+u --> UC2
+u --> UC3
+UC2 <.. UC4: extend
+u --> UC5
+UC5 ..> UC6: include
+UC5 ..> UC7: include
+UC5 ..> UC8: include
+u --> UC9
+u --> UC10
+UC10 <.. UC11: extend
+s --> UC12
+s --> UC13
+
+@enduml
+
+
+```
+
+## Описание алгоритма
+
+TBD
---
+++ my-website/docusaurus.config.js
@@ -0,0 +1,130 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+
+const simplePlantUML = require("@akebifiky/remark-simple-plantuml"); // объявляем плагин для plantuml
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Course project', //Название сайта на начальной странице
+  tagline: 'About course project', // Описание сайта на начальной странице
+  favicon: 'img/favicon.ico', // Иконка сайта
+  url: 'https://nstsai.github.io/', // Адрес gitlab pages, обычно https://<username>.github.io
+  baseUrl: 'SAproject', // Базовый url, обычно название репозитория
+  organizationName: 'nstsai', // Имя GitHub пользователя
+  projectName: 'SAproject', // Имя репозитория
+  onBrokenLinks: 'warn', // Чтобы не падать при наличии неработающих ссылок
+  onBrokenMarkdownLinks: 'warn', // Чтобы не падать при наличии неработающих ссылок
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
+
+//подключаем плагин для drawio
+  plugins: [
+    ['drawio', {}]
+  ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: 'docs',
+          editUrl:
+            'https://github.com/nstsai/SAproject',
+          remarkPlugins: [simplePlantUML], //подключаем плагин для plantuml
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+    // подключаем плагин для OPENAPI
+    [
+      'redocusaurus',
+      {
+        specs: [
+          {
+            id: 'petstore',
+            spec: 'api_specs/openapi.yaml',
+          },
+        ],
+        theme: {
+          primaryColor: '#1890ff',
+        },
+      }
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'doc-as-a-code-workshop', //Название на навбаре
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg', //Логотип на навбаре
+        },
+        // тут можно настроить элементы навбара
+        items: [
+          {
+            href: 'https://github.com/nstsai/SAproject',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      // тут можно настроить элементы футера
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Docs',
+                to: '/docs/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
